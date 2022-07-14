1505c3e8a3ef6620413ed7b68495d95ba97b9e17
DeepEdu <ideepikakmri@gmail.com>
2022-07-14 19:41:13 +0530

    updated some function

--- src/route/route.question.js
+++ src/route/route.question.js
@@ -78,53 +78,81 @@ questionRoute.route("/add-question").post((req, res, next) => {
     // quizQuestionCount is total question to be taken in Quiz
     var quizQuestionCount = req.body.count;
     const quizList = [];
-    distribution
-    .find({userId: usrId},(error,data) => {
-      if(error){
-        console.log(error);
-        return next(error);
-      }
-      else{
-        // Function to find total number of Question of the user
-        console.log("Range of the user are " + data);
-        var totalQuestion =  totalNumberOfQuestion(data);
-
-        for(let i =0; i<data.length; i++){
-
-          // Equity is total Question from each Range to be in Quiz
-          let equity = (data[i].countQuestion*quizQuestionCount)/totalQuestion;
-          const temp = data[i];
-          // Rounding Off equity to get the Question
-          equity = Math.round(equity);
-          // Find QuestionId of the Question which are in the given range
-          quiz
-          .find({'userId': usrId, 'expertiseLevel' :{ $gt : temp.RangeMin, $lte: temp.RangeMax} })
-          .limit(equity)
-          .then(async(quizQuestion) => {
-            for (let i = 0; i < quizQuestion.length; i++) {
-              let qesId = quizQuestion[i].quesId;
-              // To find all Question with the given question Id
-              var theQuestion = await inQuestionCollection(qesId);
-              // pushing Question to QuizList
-              quizList.push(
-                {
-                  QuestionId:theQuestion.questionId,
-                  Question: theQuestion.ques,
-                  Answer: theQuestion.ans
-                }); 
-                // if total Question length is greater then the quizQuestionCount 
-                if(quizList.length == quizQuestionCount){
-                  console.log(" QuizList Length is:"+ quizList.length);
-                 res.json(quizList);  
-                 return;
-                }
-              }
-            })
-          }
+    distribution.aggregate([
+      { 
+        $match: {
+          userId: usrId
         }
+        },{
+        $group: { 
+          _id: "$userId", 
+          total: { 
+              $sum: "$countQuestion"
+            } 
+          }
+        }], function(error, totques) {
+          const lenQuestion = totques[0].total;
+        distribution
+        .find({userId: usrId},(error,data) => {
+          if(error){
+            console.log(error);
+            return next(error);
+          }
+          else{
+          // Function to find total number of Question of the user
+          console.log("Range of the user are " + data);
+          if(data.length == 0) {
+            console.log(" ERROR: Data Not found" + data.length);
+            return next(error);
+          }
+          for(let i =0; i<data.length; i++){
+
+            // Equity is total Question from each Range to be in Quiz
+            let equity = (data[i].countQuestion*quizQuestionCount)/lenQuestion;
+            console.log("equity" +equity)
+            const temp = data[i];
+            // Rounding Off equity to get the Question
+            equity = Math.round(equity);
+
+            // Find equity number of QuestionIds from quiz collection, 
+            // whose expertiseLevel is in the current range.
+            quiz
+            .find({'userId': usrId, 'expertiseLevel' :{ $gt : temp.RangeMin, $lte: temp.RangeMax} })
+            .limit(equity)
+            .then(async(quizQuestion) => {
+              for (let i = 0; i < quizQuestion.length; i++) {
+                let qesId = quizQuestion[i].quesId;
+                // To find all Question with the given question Id
+                var theQuestion = await findInQuestionCollection(qesId);
+                // pushing Question to QuizList
+                quizList.push( schemaForQuiz(theQuestion) );
+                  console.log(quizList);
+                  /** 
+                   * if total Question length for Quiz is equal to 
+                   * the length of the question present in all range for the user 
+                  */
+                  if(quizList.length == quizQuestionCount){
+                    console.log(" QuizList Length is:"+ quizList.length);
+                    res.json(quizList);  
+                  return;
+                  }
+                }
+              })
+            }
+          }
+        })
       })
     });
 
+
+    function schemaForQuiz(theQuestion){
+      return (
+        {
+          QuestionId:theQuestion.questionId,
+          Question: theQuestion.ques,
+          Answer: theQuestion.ans
+        }); 
+    }
   // function to find Total number of Question in
   function totalNumberOfQuestion(data){
     var totalSum =0;
@@ -135,7 +163,7 @@ questionRoute.route("/add-question").post((req, res, next) => {
   }
   
   // function to find the Question from Qid in the Question Collection
-  async function inQuestionCollection(qesId) {
+  async function findInQuestionCollection(qesId) {
     return question
     .findOne({questionId: qesId})
     .then(( oneQuestion, error, next) => {
